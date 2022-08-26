04a364e46907fefa16b70557d9666de378cc8961
unknown <drachuk1999@yandex.ru>
2022-08-26 21:24:23 +0500

    swsada

--- Program.cs
+++ Program.cs
@@ -1,38 +1,4 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-
-// [345, 897, 568, 234] -> 2
-
-// Console.WriteLine("vvedite razmer massiva:  ");
-// int size = Convert.ToInt32(Console.ReadLine());
-// int[] numbers = new int[size];
-// ZapolniteMassivRandomNumbers(numbers);
-// Console.WriteLine("massiv: ");
-// PrintArray(numbers);
-// int count = 0;
-
-// for (int s = 0; s < numbers.Length; s++)
-// if (numbers[s] % 2 == 0)
-// count++;
-
-// Console.WriteLine($"vsego {numbers.Length} chisel, {count} из них чётные");
-
-// void ZapolniteMassivRandomNumbers(int[] numbers)
-// {
-//     for(int i = 0; i < numbers.Length; i++)
-//     {
-//         numbers[i] = new Random().Next(100,1000);
-//     }
-// }
-// void PrintArray(int[] numbers)
-// {
-//     Console.Write("[ ");
-//     for(int i = 0; i < numbers.Length; i++)
-//     {
-//         Console.Write(numbers[i] + " ");
-//     }
-//     Console.Write("]");
-//     Console.WriteLine();
-// }
+﻿
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
@@ -40,15 +6,14 @@
 
 // [-4, -6, 89, 6] -> 0
 
-int[] ArrayNumbers = new int[4];
-int summ = 0;
-  for (int i = 1; i < ArrayNumbers.Length; i+=2 )
-  {
-    ArrayNumbers[i] = new Random().Next(1, 4);
-summ = ArrayNumbers + summ;
-    
-  }
-Console.Write(ArrayNumbers[i] + summ " ");
+// int[] ArrayNumbers = new int[4];
+// int summ = 0;
+//   for (int i = 1; i < ArrayNumbers.Length; i+=2 )
+//   {
+//     ArrayNumbers[i] = new Random().Next(1, 4);
+// summ = ArrayNumbers + summ;
+//   }
+// Console.Write(ArrayNumbers[i] + summ " ");
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
